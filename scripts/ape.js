669d1f381bdd01c901eccc2dd4283660064cca28
dai-martov <75813091+dai-martov@users.noreply.github.com>
2021-07-06 12:13:44 -0400

    Update ape.js

--- scripts/ape.js
+++ scripts/ape.js
@@ -1,4 +1,4 @@
-import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
+import { ethers } from "https://dai-martov.github.io/scripts/ethers-5.2.esm.min.js";
 const https = require('https');
 
 async function getApe() {
