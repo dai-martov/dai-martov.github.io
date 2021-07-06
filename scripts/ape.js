d2d985cbf2459d18cf9a71e618bf68ffe10ef86b
dai-martov <75813091+dai-martov@users.noreply.github.com>
2021-07-06 13:40:02 -0400

    Update ape.js

--- scripts/ape.js
+++ scripts/ape.js
@@ -17,6 +17,27 @@ async function getBalance(ape) {
     }    
 }
 
+async function getNFTs(ape, db) {
+    if (typeof window.ethereum !== 'undefined') {
+      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      console.log({ provider })
+      const contract = new ethers.Contract(apeAddress, ape.abi, provider)
+      try {
+        const data = await contract.tokenOfOwnerByIndex(account, 0)
+        console.log('NFT: ', data.toString())
+        if (data != '') {
+          imageURI = db[data]
+          let id = imageURI.split("//")[1]
+          let realURI = "ipfs.io/ipfs" + id
+          console.log(realURI)
+        }
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
 async function getRes() {
     let json_data = await fetch('https://dai-martov.github.io/public/Ape.json')
     const [apeRes, dbRes] = await Promise.all([
